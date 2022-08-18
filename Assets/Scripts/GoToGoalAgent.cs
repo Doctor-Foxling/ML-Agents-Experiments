68ceaa50b782575381fc4b4c7a79ee869714886b
Hamza Khan <son.of.mithris@gmail.com>
2022-08-18 16:29:42 +0100

    Initial Psuh

---
+++ Assets/Scripts/GoToGoalAgent.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class GoToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+
+    private Vector3 localPosition;
+    private Vector3 targetLocalPos;
+    private void Start()
+    {
+        localPosition = transform.localPosition;
+        targetLocalPos = targetTransform.localPosition;
+    }
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = localPosition + new Vector3(Random.Range(-3f, 1f), 0, Random.Range(-3f, 3f));
+        targetTransform.localPosition = targetLocalPos + new Vector3(Random.Range(-4f, 1f), 0, Random.Range(-2.5f, 2.5f)); 
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 5f;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    //Here we can essentially modify the actions that would then be recieved by the OnActionRecieved function
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+1f);
+            //The Episode should end when the agent recieves the final reward, or loses
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
+    }
+}
